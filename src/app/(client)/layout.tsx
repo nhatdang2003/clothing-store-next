c7e63aacbb0ccab14340e3382892dcf3acfeb027
Nhat Dang <nhatdzfbk@gmail.com>
2025-06-14 06:47:32 +0700

    feat: add metadata for Ez Store with title and description in layout component

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,6 +1,12 @@
 import Navigation from "@/components/navigation/navbar";
 import { Footer } from "@/components/footer/footer";
 import ChatbotWidget from "@/components/chatbot/ChatbotWidget";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Ez Store",
+    description: "Ez Store cửa hàng thời trang uy tín",
+}
 
 export default function LayoutShop({ children }: { children: React.ReactNode }) {
     return (
